da80aebc7100657077a714c5740b1d46fd3697f0
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-02-25 16:15:51 -0600

    Added Nav component

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
         root: __dirname,
         // specify our components path
         alias: {
-            Main: 'app/components/Main.jsx'
+            Main: 'app/components/Main.jsx',
+            Nav: 'app/components/Nav.jsx'
         },
         extensions: ['','.js', '.jsx']
     },
