d6c16cda9e5dd2dfcfc543ab284b077e04adde8d
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-02-27 22:06:30 -0600

    updated webpack.config

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,10 +1,24 @@
 var axios = require('axios');
 
 // Make a variable that cannot be altered
-const OPEN_WEATHER_MAP_URL = '';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=bfda3060d6086f7625bfb25db8f1df54&units=imperial';
+
 
 module.exports = {
     getTemp: function (location){
+        // ES6 Template Strings
+        var encodedLocation = encodeURIComponent(location);
+        var requestURL = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
+        axios.get(requestURL).then(function (res) {
+            if(res.data.cod && res.data.message){
+                throw new Error(res.data.message);
+            } else {
+                return res.data.main.temp:
+            }
+        }, function (res) {
+            throw new Error(res.data.message);
+
+        });
     }
 }
