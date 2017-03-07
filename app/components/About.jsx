cec252b749f798215395c2849587b4952ec27cb3
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-03-07 12:39:41 -0600

    Update About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
       <div>
-          <h3>About Component</h3>
-          <p>Welcome to About page!</p>
+          <h1 className="text-center">About</h1>
+          <p>Weather App built on React. I built this while learning React from a tutorial</p>
+          <p>Technologies used:</p>
+          <ul>
+              <li>
+                  <a href="https://facebook.github.io/react">React</a> - JavaScript framework.
+              </li>
+              <li>
+                  <a href="http://openweathermap.org">Open Weather Map</a> - API used to search weather data.
+              </li>
+          </ul>
       </div>
   )
 };
