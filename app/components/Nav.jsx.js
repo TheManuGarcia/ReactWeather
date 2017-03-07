d00d18d23496fbb79826788de6d454a081aceafa
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-03-07 10:56:34 -0600

    Refactor Nav presentation component|| Switched from the stateless functional component back to the React.createClasscomponent definition

--- app/components/Nav.jsx.js
+++ app/components/Nav.jsx.js
@@ -23,10 +23,10 @@ var Nav = React.createClass({
                        <li>
                            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
                        </li>
-
                    </ul>
                </div>
                <div className="top-bar-right">
+
                    <form onSubmit={this.onSearch}>
                        <ul className="menu">
                            <li>
