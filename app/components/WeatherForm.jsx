cfa33e648bdbd945ba0210b9c23358f69120d507
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-03-09 16:21:30 -0600

    Updated styles in Examples and About.jsx

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -22,7 +22,7 @@ var WeatherForm = React.createClass({
          <div>
              {/*Every time the form is submitted onFormSubmit gets called*/}
              <form onSubmit={this.onFormSubmit}>
-                 <input type="text" ref="location"/>
+                 <input type="search" ref="location" placeholder="Search weather by city"/>
                  <button className="button expanded hollow">Get Weather</button>
              </form>
          </div>
