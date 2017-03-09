1b4d0b8745f8475b7ebe1d744db63815ed02f43b
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-03-08 23:25:03 -0600

    Updated ErrorModal.jsx with getDefaultProps and propTypes

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,16 +1,27 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+    // Define a set of Default props. If they are provided we'll use them, if not we will use our defaults.
+    getDefaultProps: function () {
+        return {
+            title: 'Error'
+        }
+    },
+    propTypes: {
+        title: React.PropTypes.string,
+        message: React.PropTypes.string.isRequired
+    },
 
     componentDidMount: function () {
       var modal = new Foundation.Reveal($('#error-modal'));
       modal.open();
     },
     render: function () {
+        var {title, message} = this.props;
         return(
           <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-              <h4>Some title</h4>
-              <p>Our Error</p>
+              <h4>{title}</h4>
+              <p>{message}</p>
               <p>
                   <button className="button hollow" data-close="">
                       Okay
