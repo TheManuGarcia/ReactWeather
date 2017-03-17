123f01beb472df7c6a946053089ba6b85a0e13a0
TheManuGarcia <jmanuel.gtez@gmail.com>
2017-03-16 21:32:40 -0500

    Deleted wrong css classes and changed small typed errors

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next){
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
-    console.log('Listening on port' + PORT);
+    console.log('Listening on port ' + PORT);
 });
 
 
